923dc740e4bc8276618988fc5afe6b5710ed3044
Dimaspanduw <95301103+Dimaspanduw@users.noreply.github.com>
2022-10-04 23:35:26 +0800

    posttest 3

---
+++ Posttest3/java.js
@@ -0,0 +1,13 @@
+alert("WELCOME TO SEAFOOD BANG BANG");
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+//hapus
+const lis= document.querySelectorAll('li');
+const list=document.querySelector('ul');
+
+//hapus list item
+lis[0].remove();
